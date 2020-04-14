c6bf65e2aa9eb23cea8dd4ecaaf0c031c84a1e16
Artideusz <materac2000@gmail.com>
2020-04-14 19:43:43 +0200

    Initial commit with readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mass WebP
+
+This is a simple mass webp converter using cwebp.
+
+Usage: mass-webp.sh <Folder with the images to convert>
+
+The images will be placed in the same folder as the original images.
---
+++ mass-webp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+print_usage() {
+    printf "Usage: $0 <directory with images to convert to WebP>";
+}
+
+if [ -d $1 ] ; then
+    files=()
+    while IFS= read -r -d $'\0'; do
+        files+=("$REPLY")
+    done < <(find $1 \( -iname \*.jpg -o -iname \*.png -o -iname \*.jpeg \) -print0)
+    
+    for i in "${files[@]}"
+    do
+        FILE="${i%.*}.webp"
+        if [ -f FILE ] ; then
+            echo "${FILE} already exists. Skipping..."
+        else
+            echo "${FILE} does not exist! Creating a webp image!"
+            cwebp $i -o $FILE
+        fi
+    done
+
+else
+    print_usage
+    exit 1
+fi
